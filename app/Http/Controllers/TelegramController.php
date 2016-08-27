b0f4875157b52cdf66633d0a30c3c320e49bad99
shavenking <shavenking@gmail.com>
2016-08-28 02:47:28 +0800

    Refactor

--- app/Http/Controllers/TelegramController.php
+++ app/Http/Controllers/TelegramController.php
@@ -3,17 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Arr;
 use Telegram\Bot\Api as Telegram;
 
 use App\Http\Requests;
 
 class TelegramController extends Controller
 {
-    public function webhook(Request $request, Telegram $telegram)
+    public function webhook(Request $request, Telegram $telegram, Arr $arr)
     {
         if ($request->has('message')) {
             $message = $request->get('message');
-            $chatId = $message['chat']['id'];
+            $chatId = $arr->get($message, 'chat.id');
 
             return response()->json([
                 'method' => 'sendMessage',
