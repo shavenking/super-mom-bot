938cec4959490f647580a6819e19cd5eabe6ada1
shavenking <shavenking@gmail.com>
2016-08-28 01:04:15 +0800

    Test if telegram bot works

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,32 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+// only the guy has token is able to set webhook
+Route::get('/set-webhook', function () {
+    $token = Request::get('token');
+
+    if (env('TELEGRAM_BOT_TOKEN') !== $token) {
+        return;
+    }
+
+    $url = route('telegram.webhook');
+
+    Telegram::setWebhook(compact('url'));
+
+    return 'ok';
+});
+
+Route::get('/webhook' . env('TELEGRAM_BOT_TOKEN'), function () {
+    if (Request::has('message')) {
+        $message = Request::get('message');
+        $chatId = Arr::get($message, 'chat.id');
+
+        Telegram::sendMessage([
+            'chat_id' => $chatId,
+            'text' => 'ok'
+        ]);
+    }
+
+    return;
+})->name('telegram.webhook');
